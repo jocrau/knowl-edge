e89b743f627e7636b897b8d48a5389fed34c5abe
Jochen Rau <jochen.rau@typoplanet.de>
2012-04-15 14:36:20 +0200

    Add more compojure dependencies

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Knowl is a Semantic Content Management System"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring "1.1.0-SNAPSHOT"]
-                 [compojure "1.0.1"]])
+                 [compojure "1.0.1"]
+                 [compojure-rest "0.2.2"]])
--- src/knowl/edge.clj
+++ src/knowl/edge.clj
@@ -1,1 +1,10 @@
-(ns knowl.edge)
+(ns knowl.edge
+  (:use
+    compojure.core
+    ring.adapter.jetty
+    ring.middleware.stacktrace
+    ring.middleware.params
+    ring.middleware.reload)
+  (:require
+    [compojure.route :as route]
+    [compojure-rest.resource :as rest]))
