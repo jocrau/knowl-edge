b7a20edda470ca81c88cbcd09e23bc8cf3abbb12
Jochen Rau <jochen.rau@typoplanet.de>
2012-04-26 11:23:03 -0400

    Upgraded to lein2

--- project.clj
+++ project.clj
@@ -1,7 +1,4 @@
-(defproject knowl "0.0.1-SNAPSHOT"
-  :description "Knowl is a Semantic Content Management System"
-  :main knowl.edge
-  :aot [knowl.edge]
+(defproject knowl "0.0.1-SNAPSHOT" 
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring "1.1.0-SNAPSHOT"]
                  [compojure "1.0.1"]
@@ -9,7 +6,13 @@
                  [re-rand "0.1.0"]
                  [clj-time "0.4.1"]
                  [org.apache.jena/jena-arq "2.9.0-incubating"]]
-  :dev-dependencies [[midje "1.3.1"]]
-  :repositories {"aduna (sesame)" "http://repo.aduna-software.org/maven2/releases/"
-                 "Jena" "https://repository.apache.org/content/repositories/releases/"}
-  :resources-path "resources/public/")
+  :profiles {:dev {:dependencies [[midje "1.3.1"]]}}
+  :repositories {"aduna (sesame)"
+                 "http://repo.aduna-software.org/maven2/releases/",
+                 "Jena"
+                 "https://repository.apache.org/content/repositories/releases/"}
+  :resource-paths ["resources/public"]
+  :aot [knowl.edge]
+  :main knowl.edge
+  :min-lein-version "2.0.0"
+  :description "Knowl is a Semantic Content Management System")
