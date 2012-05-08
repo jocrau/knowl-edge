203715b2a2469948ca9250bda0adda32a7876a85
Jochen Rau <jochen.rau@typoplanet.de>
2012-05-07 21:52:33 -0400

    Adjust project configuration for uberjar

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
   :profiles {:dev {:dependencies [[midje "1.3.1"]]}}
   :resource-paths ["resources/public"]
   :aot [knowl.edge]
-  :main knowl.edge
+  :main knowl.edge.server
+  :uberjar-exclusions [#"META-INF/ECLIPSEF.SF"]
   :min-lein-version "2.0.0"
   :description "This is a knowl:edge Management System")
