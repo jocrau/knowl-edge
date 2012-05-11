0bd3237dea8931bee5166922a01521460497f15e
Jochen Rau <jochen.rau@typoplanet.de>
2012-05-11 08:52:44 -0400

    Fix aot compile configuration

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [org.apache.jena/jena-arq "2.9.0-incubating"]]
   :profiles {:dev {:dependencies [[midje "1.3.1"]]}}
   :resource-paths ["resources/public"]
-  :aot [knowl.edge]
+  :aot [knowl.edge.server]
   :main knowl.edge.server
   :uberjar-exclusions [#"META-INF/ECLIPSEF.SF"]
   :min-lein-version "2.0.0"
