78d7492d46c2ad491184091bf0aeaf5d4da65a74
Jochen Rau <jochen.rau@typoplanet.de>
2012-06-05 09:08:24 -0400

    Set enlive version to 1.0.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [ring "1.1.0-SNAPSHOT"]
                  [compojure "1.0.1"]
-                 [enlive "1.0.0"]
+                 [enlive "1.0.1"]
                  [re-rand "0.1.0"]
                  [clj-time "0.4.1"]
                  [org.apache.jena/jena-arq "2.9.0-incubating"]]
