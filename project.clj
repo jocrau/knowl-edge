3339f24c4dfaf8484b0cd1b66a06ddfea02eefe3
Jochen Rau <jochen.rau@typoplanet.de>
2012-09-28 08:49:45 -0400

    Adjust project config

--- project.clj
+++ project.clj
@@ -7,19 +7,16 @@
                  [enlive "1.0.1"]
                  [re-rand "0.1.0"]
                  [clj-time "0.4.4"]
-                 [org.apache.jena/jena-arq "2.9.0-incubating"]]
+                 [org.apache.jena/jena-arq "2.9.0-incubating"]
+                 [rdfa "0.5.0-SNAPSHOT"]]
   :plugins [[lein-cljsbuild "0.2.7"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0"]
-                                  [rdfa "0.5.0-SNAPSHOT"]]}}
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
   :source-paths ["src/clj"]
   :resource-paths ["resources"]
   :cljsbuild {:builds [{:id "dev"
                         :source-path "src/cljs"
                         :jar true
-                        :compiler {:output-to "resources/public/js/app.js"
-                                   :optimizations :whitespace
-                                   :pretty-print true
-                                   :print-input-delimiter true}}
+                        :compiler {:output-to "resources/public/js/app.js"}}
                        {:id "prod"
                         :source-path "src/cljs"
                         :jar true
