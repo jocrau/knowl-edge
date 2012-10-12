d1a0c526a332ab0f90574be3d8b5a38070a0aadc
Jochen Rau <jochen.rau@typoplanet.de>
2012-10-12 12:14:25 -0400

    Set group id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject knowledge "0.0.1-SNAPSHOT"
+(defproject org.knowl-edge/knowledge "0.0.1-SNAPSHOT"
   :description "This is a knowl:edge Management System"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
@@ -8,7 +8,7 @@
                  [re-rand "0.1.0"]
                  [clj-time "0.4.4"]
                  [org.apache.jena/jena-arq "2.9.0-incubating"]
-                 [rdfa "0.5.0-SNAPSHOT"]]
+                 [org.knowl-edge/rdfa "0.5.0-SNAPSHOT"]]
   :plugins [[lein-cljsbuild "0.2.7"]]
   :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
   :source-paths ["src/clj"]
