fa2f1a4f98498b4afb99e24f53bce1057e55e517
Jochen Rau <jochen.rau@typoplanet.de>
2012-11-14 16:51:35 -0500

    Expose RDFaAPI to JavaScript

--- src/cljs/knowledge/rdfa/api.cljs
+++ src/cljs/knowledge/rdfa/api.cljs
@@ -2,7 +2,15 @@
   (:require [knowledge.store :as store]
             [knowledge.base :as base]))
 
+;; Data Access
+
 (defn find-by-query [query callback] (store/find-by-query base/default-store query callback))
 
+;; DocumentData (see http://www.w3.org/TR/rdfa-api/#document-data)
+
+;; DataDocument (see http://www.w3.org/TR/rdfa-api/#the-document-interface)
+
+;; Expose the API to JavaScript
+
 (set! js/document.data (fn []))
-(set! js/document.data.find-by-query find-by-query)
+(set! js/document.data.query find-by-query)
