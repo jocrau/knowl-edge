58b95ca8b0bd48c06192911230f477178754bf33
Jochen Rau <jochen.rau@typoplanet.de>
2012-11-15 22:10:52 -0500

    Add init to bakepedia.application

--- src/cljs/bakepedia/application.cljs
+++ src/cljs/bakepedia/application.cljs
@@ -1,3 +1,9 @@
 (ns bakepedia.application
   (:require [cljs.core :as cljs]
-            [knowledge.base :as base]))
+            [knowledge.rdfa.api :as api]
+            [knowledge.effects :as effects]))
+
+#_(defn init []
+  (effects/hide (api/get-elements-by-property "http://www.w3.org/ns/ma-ont#hasFragment")))
+
+(.addEventListener js/document "DOMContentLoaded" init)
