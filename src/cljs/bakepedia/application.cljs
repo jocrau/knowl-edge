2db321067de6f35df305677d78c6f5f030bcf639
Jochen Rau <jochen.rau@typoplanet.de>
2012-11-19 11:47:39 -0500

    Fix a problem with bakepedia app init

--- src/cljs/bakepedia/application.cljs
+++ src/cljs/bakepedia/application.cljs
@@ -3,7 +3,7 @@
             [knowledge.rdfa.api :as api]
             [knowledge.effects :as effects]))
 
-#_(defn init []
-  (effects/hide (api/get-elements-by-property "http://www.w3.org/ns/ma-ont#hasFragment")))
+(defn init []
+  #_(effects/hide (api/get-elements-by-property "http://www.w3.org/ns/ma-ont#hasFragment")))
 
 (.addEventListener js/document "DOMContentLoaded" init)
